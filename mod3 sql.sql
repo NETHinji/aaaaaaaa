651178d2c14222de44bb7af97bc9cace566185d0
NETHinji <44230892+NETHinji@users.noreply.github.com>
2018-11-28 10:38:43 +0530

    Add files via upload

--- mod3 sql.sql
+++ mod3 sql.sql
@@ -25,3 +25,48 @@ travelerStateId int foreign key references mod3.travelerState(stateId),
 travelerCityId int foreign key references mod3.travelerCity(cityId),
 travelerAddress varchar(100)
 )
+
+select * from mod3.travelerState
+select * from mod3.travelerCity
+select * from mod3.travelerAccomodation
+
+
+create proc mod3.uspAddTravelerDetails(@travelerName varchar(30), @travelerCID date, @travelerCOD date, @travelerStateId int, @travelerCityId int, @travelerAddress varchar(100))
+as
+begin
+insert into mod3.travelerAccomodation values (@travelerName, @travelerCID, @travelerCOD, @travelerStateId, @travelerCityId, @travelerAddress)
+end
+go
+
+
+create proc mod3.uspDeleteTravelerDetails(@travelerID int)
+as
+begin
+delete from mod3.travelerAccomodation where travelerID=@travelerID
+end
+go
+
+
+create proc mod3.uspUpdateTravelerDetails(@travelerID int, @travelerName varchar(30), @travelerCID date, @travelerCOD date, @travelerStateId int, @travelerCityId int, @travelerAddress varchar(100)) 
+as
+begin
+update mod3.travelerAccomodation
+set travelerName=@travelerName, travelerCID=@travelerCID, travelerCOD=@travelerCOD, travelerStateId=@travelerStateId, travelerCityId=@travelerCityId, travelerAddress=@travelerAddress
+end
+go
+
+
+create proc.uspShowTravelerDetailsByID(@travelerID int)
+as
+begin
+select * from mod3.travelerAccomodation where travelerID=@travelerID
+end
+go
+
+
+create proc.uspShowAllTravelerDetails(@travelerID int)
+as
+begin
+select * from mod3.travelerAccomodation
+end
+go
